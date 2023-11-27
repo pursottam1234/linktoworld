a9f1004e90d08dc80ddf178ba18ce44472220e8e
pursottam1234 <purush.bhandari69@gmail.com>
2023-11-27 12:58:34 +0545

    added sg

--- main.tf
+++ main.tf
@@ -53,3 +53,30 @@ resource "aws_route_table_association" "routetbassoc01" {
   subnet_id      = aws_subnet.sub-us-east-1a.id
   route_table_id = aws_route_table.routetb01.id
 }
+
+resource "aws_security_group" "sg01" {
+  name_prefix = "sg01_security_group"
+  vpc_id = aws_vpc.avpctesting.id
+
+ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+egress {   
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+}
+}
